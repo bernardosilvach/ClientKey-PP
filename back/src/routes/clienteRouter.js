9baeb7cd1c2891c83ce6ae923ba89b0e6317a562
Bernardo <04968989067@senacrs.edu.br>
2024-10-09 12:11:27 -0300

    adicionei o swagger

--- back/src/routes/clienteRouter.js
+++ back/src/routes/clienteRouter.js
@@ -4,9 +4,78 @@ const router = Router();
 
 const {storeClientes, mostrarClientes, deletarClientes, updateCliente} = require('../controller/clientesController');
 
+/**
+* @swagger
+*  /store/clientes:
+*   post:
+*     summary: Cria clientes
+*     responses:
+*       200:
+*         description: Cria os clientes na database
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
+
 router.post('/store/clientes', storeClientes);
+
+/**
+* @swagger
+* /store/mostrarClientes:
+*   post:
+*     summary: Mostra a lista de clientes
+*     responses:
+*       200:
+*         description: Retorna a lista de clientes do database
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
+
 router.post('/store/mostrarClientes', mostrarClientes);
+
+/**
+* @swagger
+* /store/deletarClientes/:id:
+*   delete:
+*     summary: Deleta os clientes
+*     responses:
+*       200:
+*         description: Deleta os clientes da database
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
 router.delete('/store/deletarClientes/:id', deletarClientes);
+
+/**
+* @swagger
+* /store/clientes/:id:
+*   put:
+*     summary: Deleta os clientes
+*     responses:
+*       200:
+*         description: Deleta os clientes da database
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
 router.put('/store/clientes/:id', updateCliente);
 
 module.exports = router;
--- back/src/server.js
+++ back/src/server.js
@@ -1,4 +1,23 @@
 const app = require('./app');
 const port = app.get('port');
 
+const swaggerUi =  require('swagger-ui-express')
+const swaggerJsDoc =  require('swagger-jsdoc')
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API das Funções do ClientKey",
+            version: "1.0.0",
+            description: "API CRUD para gerenciar as funções",
+        },
+        servers: [{url:  "http://localhost:3000"}],
+    },
+    apis: [`${__dirname}/routes/*.js`], //caminho para as rotas
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.listen(port, () => console.log(`Run on port ${port}!`));
