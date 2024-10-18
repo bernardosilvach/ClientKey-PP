c9158051bdcb851094d285de666c8b555742d7b0
bernardo <bernardosilvach2@gmail.com>
2024-10-17 22:15:39 -0300

    atuliazacao no perfil do usuario no banco de dados

--- back/src/controller/usersController.js
+++ back/src/controller/usersController.js
@@ -30,31 +30,56 @@ async function storeUsers(request, response) {
 }
 
 async function Login(request, response) {
-    const params = Array(
-        request.body.email,
-        request.body.senha,
-    )
+    const params = [request.body.email, request.body.senha];
 
-    const query = "SELECT email, senha, id, nome, nomenegocio FROM users WHERE email = ? AND senha = ?";
+    const query = "SELECT id, nome, cpf, email, nomenegocio FROM users WHERE email = ? AND senha = ?";
     
     connection.query(query, params, (err, results) => {
-        if(results && results.length > 0) {
+        if (results && results.length > 0) {
             response.status(200).json({
                 success: true,
-                message: "Sucesso!",
-                data: results
-            })
+                message: "Login feito com sucesso!",
+                data: results // Enviando os dados do usuário (id, nome, cpf, email, nome_negocio)
+            });
         } else {
             response.status(400).json({
                 success: false,
-                message: "Erro!",
-                sql: err,
-            })
+                message: "Erro no login.",
+                sql: err
+            });
         }
-    })
+    });
 }
+
+
+async function updateUser(request, response) {
+    const userId = request.params.id; // ID do usuário a ser atualizado
+    const { nome, email, cpf, nomeNegocio } = request.body; // Dados enviados no corpo da requisição
+
+    // Prepara a query SQL de atualização
+    const query = "UPDATE users SET nome = ?, email = ?, cpf = ?, nomenegocio = ? WHERE id = ?";
+    const params = [nome, email, cpf, nomeNegocio, userId]; // Parâmetros para a query
+
+    // Executa a query no banco de dados
+    connection.query(query, params, (err, results) => {
+        if (results) {
+            response.status(200).json({
+                success: true,
+                message: "Usuário atualizado com sucesso!"
+            });
+        } else {
+            response.status(400).json({
+                success: false,
+                message: "Erro ao atualizar usuário.",
+                sql: err
+            });
+        }
+    });
+}
+
 
 module.exports = {
     storeUsers,
-    Login
+    Login,
+    updateUser
 }
--- back/src/routes/usersRouter.js
+++ back/src/routes/usersRouter.js
@@ -2,9 +2,10 @@ const {Router} = require('express');
 
 const router = Router();
 
-const {storeUsers, Login} = require('../controller/usersController');
+const {storeUsers, Login, updateUser} = require('../controller/usersController');
 
 router.post('/store/users', storeUsers);
 router.post('/store/login', Login);
+router.put('/store/users/:id', updateUser);
 
 module.exports = router;
