0d0e55e6c3697b74adef38c4577471a69c9800f8
Bernardo <04968989067@senacrs.edu.br>
2024-10-23 11:23:16 -0300

    atualização do swagger e perfil de usuario

--- back/src/routes/usersRouter.js
+++ back/src/routes/usersRouter.js
@@ -4,8 +4,60 @@ const router = Router();
 
 const {storeUsers, Login, updateUser} = require('../controller/usersController');
 
+/**
+* @swagger
+*  /store/users:
+*   post:
+*     summary: Cria os usuários
+*     responses:
+*       200:
+*         description: Cria os usuários na database
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
+
 router.post('/store/users', storeUsers);
+
+/**
+* @swagger
+*  /store/login:
+*   post:
+*     summary: Recebe os dados de login
+*     responses:
+*       200:
+*         description: Recebe e valida os dados de login no database
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
+
 router.post('/store/login', Login);
+
+/**
+* @swagger
+*  /store/users/:id:
+*   put:
+*     summary: Atualização do cadastro de usuário
+*     responses:
+*       200:
+*         description: Faz a atualização do cadastro do usuário no database
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
 router.put('/store/users/:id', updateUser);
 
 module.exports = router;
